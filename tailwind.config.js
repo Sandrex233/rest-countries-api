00168cee960845e6050b25c260bb82e3c890bdf5
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-10-02 18:08:10 +0400

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nurito: ["Nunito Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
